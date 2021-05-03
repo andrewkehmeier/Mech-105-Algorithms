e5b6e43c1c4d23b94773c53afe15ac68d9d12ec3
Andrew Kehmeier <83362614+andrewkehmeier@users.noreply.github.com>
2021-05-02 18:37:42 -0600

    Add files via upload

---
+++ Binary Converter/binaryConverter.m
@@ -0,0 +1,29 @@
+function [base2] = binaryConverter(base10)
+%binary A simple function to convert a base10 number to binary
+%   Inputs:
+%       base10 - A number in base10
+%   Outputs:
+%       base2 - The base10 number converted to binary
+b=2;
+r=0;
+index=1;
+base2=0;
+if base10<0
+    error('Function only takes positive numbers')
+end
+if base10==0
+    base2=base10;
+end
+if base10==1
+    base2=base10;
+end
+while base10>0
+    r=mod(base10,b);
+    base10=floor(base10/b);
+    base2(index)=r;
+    index=index+1;
+end
+base2=flip(base2);
+
+
+end
