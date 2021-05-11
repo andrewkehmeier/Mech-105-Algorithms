f5bc6b6fff3f2f808e556e37c2d1c21fb27ad177
Andrew Kehmeier <83362614+andrewkehmeier@users.noreply.github.com>
2021-05-10 20:32:39 -0600

    added cd capabilities

--- Linear Regression/linreg.m
+++ Linear Regression/linreg.m
@@ -3,7 +3,8 @@ function [m,y0] = linreg(x,y)
 %equations
 %   linreg takes two vector inputs and uses these inputs alongside the
 %   normal equations to calculate the slope and y intercept of the best fit
-%   line for the tabulated data
+%   line for the tabulated data.  Function also utilizes calculated best
+%   fit equation to calculate the coefficient of determination.
 xb=mean(x);
 yb=mean(y);
 n=length(x);
@@ -12,5 +13,9 @@ if size(x)~= size(y)
 end
 m=((sum((x-xb).*(y-yb)))./(sum((x-xb).^2)));
 y0=sum(y-m*x)/n;
+f=@(x)m.*x+y0;
+St=sum((y-yb).^2);
+Sr=sum((y-f(x)).^2);
+cd=(St-Sr)/St;
 end
 
